90fefab1480c2e8ef959cb0087b529447f5a3354
Svanik-Jain <130584200+Svanik-Jain@users.noreply.github.com>
2023-04-26 15:51:47 +0530

    Loop added

--- PeriodTeller.py
+++ PeriodTeller.py
@@ -1,26 +1,39 @@
 import pyttsx3
 from datetime import datetime
+import signal
 
-TimeNow = datetime.now()
-SayTimes = ["7:30","8:10","8:45","9:05","9:40","10:15","10:50","11:25","12:00","12:35","13:10","13:45","14:20"]
+SayTimes = ["7:30:00","8:10:00","8:45:00","9:05:00","9:40:00","10:15:00","10:50:00","11:25:00","12:00:00","12:35:00","13:10:00","13:45:00","14:20:00"]
 TextToSpeech = pyttsx3.init()
 period = 0
-CurrentTime = TimeNow.strftime("%H:%M")
+stop = False
 
-for timeI in SayTimes:
-    period += 1
-    if(timeI == "8:45"or timeI == "11:25"):
-        period -=1
-    if(CurrentTime == "8:45" or CurrentTime == "11:25"):
-        TextToSpeech.say(f"The time is {CurrentTime} and the break has started.")
-        TextToSpeech.runAndWait()
+def stopTheCode(signal,frame):
+    global stop
+    stop = True
+
+signal.signal(signal.SIGINT, stopTheCode)
+
+stop = False
+
+while True:
+    TimeNow = datetime.now()
+    CurrentTime = TimeNow.strftime("%H:%M:%S")
+    period = 0
+    for timeI in SayTimes:
+        period += 1
+        if(timeI == "8:45:00"or timeI == "11:25:00"):
+            period -=1
+        if(CurrentTime == "8:45:00" or CurrentTime == "11:25:00"):
+            TextToSpeech.say(f"The time is {CurrentTime} and the break has started.")
+            TextToSpeech.runAndWait()
+            break
+        elif(CurrentTime == "14:20"):
+            TextToSpeech.say(f"The time is {CurrentTime} and the school is over.")
+            TextToSpeech.runAndWait()
+            break
+        elif(CurrentTime==timeI):
+            TextToSpeech.say(f"The time is {CurrentTime} and the period number is {period}.")
+            TextToSpeech.runAndWait()
+            break
+    if(stop):
         break
-    elif(CurrentTime == "14:20"):
-        TextToSpeech.say(f"The time is {CurrentTime} and the school is over.")
-        TextToSpeech.runAndWait()
-        break
-    elif(CurrentTime==timeI):
-        TextToSpeech.say(f"The time is {CurrentTime} and the period number is {period}.")
-        TextToSpeech.runAndWait()
-        break
-    
